7af96f64150c87e02e2e750b01642631a6d3be63
Lucas Helamã <lhelama@gmail.com>
2024-08-22 14:26:48 -0300

    primeiro commit do velocimetro

---
+++ speedometer.js
@@ -0,0 +1,44 @@
+const speedElement = document.querySelector("#speed")
+const startBtn = document.querySelector("#start")
+const stopBtn = document.querySelector("#stop")
+
+let watchID = null
+let currentRide = null
+
+startBtn.addEventListener("click", () => {
+    if(watchID)
+        return
+
+    function handleSuccess (position){
+
+        addPosition(currentRide, position)
+        console.log(position)
+        speedElement.innerText = position.coords.speed ? (position.coords.speed * 3.6).toFixed(1) : 0
+    }
+
+    function handleError (error) {
+        console.log(error.msg)
+    }
+
+    const options = {enableHighAccuracy:true}
+
+    currentRide = createNewRide()
+
+    watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, options)
+    startBtn.classList.add("d-none")
+    stopBtn.classList.remove("d-none")
+
+})
+
+stopBtn.addEventListener("click", () => {
+    if(!watchID)
+        return
+
+    navigator.geolocation.clearWatch(watchID)
+    watchID = null
+    updateStopTime(currenteRide)
+    currentRide = null
+    stopBtn.classList.add("d-none")
+    startBtn.classList.remove("d-none")
+
+})
